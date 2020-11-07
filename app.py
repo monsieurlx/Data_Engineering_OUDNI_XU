0e57ec3fc919fc457eab901f02ab9c5265cca9d2
mrlx <leoixeu@hotmail.fr>
2020-11-07 13:58:55 +0100

    app.py updated

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 from redis import Redis, RedisError, StrictRedis
+from flask import request
 
 app = Flask(__name__)
 
-	
 @app.route('/')
 def index():
 	return render_template("index.html")
@@ -14,5 +14,7 @@ def add_text():
 	return text
 
 if __name__ == '__main__':
+	app.debug = True
 	redis_client = StrictRedis(host='redis', port=6379)
 	app.run(host='0.0.0.0')
+	
