0c94758c65c48f5fcab9ca3387d7172799d2b712
mrlx <leoixeu@hotmail.fr>
2020-11-11 16:42:06 +0100

    Update

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 from redis import Redis, RedisError, StrictRedis
 from flask import request
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+model = SentimentIntensityAnalyzer()
+
 
 app = Flask(__name__)
+
+def sentiment_analyzer_scores(sentence):
+	score = model.polarity_scores(sentence)
+	return "{:-<40} {}".format(sentence,score)
 
 @app.route('/')
 def index():
@@ -11,10 +18,10 @@ def index():
 @app.route('/', methods=['POST'])
 def add_text():
 	text = request.form['user_text']
-	return text
+	return sentiment_analyzer_scores(text)
 
 if __name__ == '__main__':
 	app.debug = True
-	redis_client = StrictRedis(host='redis', port=6379)
+	redis_client = StrictRedis(host='redis', port=5000)
 	app.run(host='0.0.0.0')
 	
